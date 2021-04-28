4806d598b289c53e02030c3c458affca0e2856ab
foshanxiaoyu <50038660+foshanxiaoyu@users.noreply.github.com>
2021-04-28 14:28:19 +0800

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,22 @@
+let classA = ['xiaohuan','xiaomin','xiaohua']
+console.log(classA)
+
+classA.push('李明想')
+console.log(classA)
+console.log(classA.length)
+console.log(classA[0])
+
+const card = {
+    name: 'my name is XXX',
+    addres:'i addre is XXXX',
+    age:888,
+}
+console.log(card.name)
+
+const post = {
+    image:'https://me.google.com/api',
+    desc:'asdfasdf',
+    date:'2020.04.15',
+    comment:['high','wrong','eessx']
+}
+console.log(post.comment[1])
