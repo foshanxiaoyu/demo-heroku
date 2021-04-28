150758a87f22b109c69e5840de5fef51f0f986c2
foshanxiaoyu <50038660+foshanxiaoyu@users.noreply.github.com>
2021-04-28 14:29:39 +0800

    Create index4.js

---
+++ index4.js
@@ -0,0 +1,10 @@
+// window.alert("You can")
+// document
+window.addEventListener('load', function(){
+    const p1 = document.getElementById('hell')
+    // console.log(p1 )
+    p1.innerText = '你来了又怎样'
+    const p2 = document.getElementById('box1')
+    p2.innerHTML= `<p id="hx">TEST</p>`
+
+})
