d18ef3757c64479aeb2c89cd1f7514199ea52bbe
foshanxiaoyu <50038660+foshanxiaoyu@users.noreply.github.com>
2021-04-28 14:27:46 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+let myData = 100
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
+console.log(myData)
+
+// function addKet(){
+//     for (let x=1; x<10; x++) {
+//         for (let y=1; y<x+1; y++) {
+//             let sumA = x*y
+//             console.log("y:",y)
+//             // console.log('x',x,'* y',y,'=',sumA)    
+//             console.log('=',sumA)    
+//         }        
+//     }        
+// }
+// addKet()
+
+// if(condition){
+//     pass
+// }else(condition){
+//     pass
+// }
+// switch(condition){
+//     case  value: 
+//         pass
+//         break;
+//     case  value: 
+//         pass
+//         break;
+//     case  value: 
+//         pass
+//         break;
+//     default:
+//         pass
+//         break;
+// }
+
+// class Person{
+//     name ='';
+//     age =100;
+//     getName(){
+//         return this.name;
+//     }
+//     getAge(){
+//         return this.age;
+//     }
+
+// }
